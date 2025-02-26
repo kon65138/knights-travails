efc6032056dfabe8e4ef9489c384354e8e3f4562
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-02-26 21:46:52 +0000

    impliment bread first search to find amount of moves to reach end

--- src/index.js
+++ src/index.js
@@ -18,15 +18,51 @@ function knightMoves(start, end) {
   //que of positions to explore
 
   let board = [];
+  makeBoard(board);
+
+  board[start[0]][start[1]] = 1;
+  // make board of visited positions
+  console.log(board);
+  let movePath = [];
+  makeBoard(movePath);
+
+  while (q.length) {
+    let current = q.shift();
+    if (end[0] === current[0] && end[1] === current[1]) {
+      return movePath[current[0]][current[1]];
+    }
+    for (let i = 0; i < 8; i++) {
+      let newPos = [
+        current[0] + knightRules[i][0],
+        current[1] + knightRules[i][1],
+      ];
+      if (onBoard(newPos) && board[newPos[0]][newPos[1]] === 0) {
+        board[newPos[0]][newPos[1]] = 1;
+        movePath[newPos[0]][newPos[1]] = movePath[current[0]][current[1]] + 1;
+        q.push(newPos);
+      }
+    }
+  }
+  return -1;
+}
+
+function makeBoard(board) {
   for (let i = 0; i < 8; i++) {
     board[i] = [];
     for (let j = 0; j < 8; j++) {
       board[i].push(0);
     }
   }
-  board[start[0]][start[1]] = 1;
-  // make board of visited positions
-  console.log(board);
+  return board;
 }
 
-knightMoves([0, 0], [7, 7]);
+function onBoard(coords) {
+  if (0 <= coords[0] && coords[0] < 8) {
+    if (0 <= coords[1] && coords[1] < 8) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(knightMoves([0, 0], [7, 7]));
