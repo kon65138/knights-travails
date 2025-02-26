2533650c00fe831aea890912338abd9a9b27d5dd
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-02-26 20:48:56 +0000

    new approach, define knights moves add que and visited board

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 import './style.css';
 
 function knightMoves(start, end) {
-  let possibleMovesBoard = [];
-  findAllEdges(possibleMovesBoard, start, end);
-  let moves = [];
-  console.log(possibleMovesBoard);
-}
+  let knightRules = [
+    [1, 2],
+    [2, 1],
+    [2, -1],
+    [1, -2],
+    [-1, -2],
+    [-2, -1],
+    [-2, 1],
+    [-1, 2],
+  ]; // all move a knight can make from current position
+  // [x(positive is right), y(positive is up)]
 
-function findAllEdges(board, start, end) {
+  let q = [];
+  q.push(start);
+  //que of positions to explore
+
+  let board = [];
   for (let i = 0; i < 8; i++) {
     board[i] = [];
     for (let j = 0; j < 8; j++) {
       board[i].push(0);
     }
   }
-  board[start[0]][start[1]] = 'start';
-  board[end[0]][end[1]] = 'end';
-  return board;
+  board[start[0]][start[1]] = 1;
+  // make board of visited positions
+  console.log(board);
 }
 
 knightMoves([0, 0], [7, 7]);
